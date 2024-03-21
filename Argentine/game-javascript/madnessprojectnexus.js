42b6e6a652a316491b4bb674752e4967d486bdd8
DanTruongForgames <157194080+DanTruongForgames@users.noreply.github.com>
2024-03-21 08:08:07 -0400

    Add files via upload

---
+++ Argentine/game-javascript/madnessprojectnexus.js
@@ -0,0 +1,28 @@
+       // Function to open a new tab with the iframe
+        function openEmbed() {
+            // Create a new window/tab
+            var newWindow = window.open('about:blank');
+
+            // Create an iframe element
+            var iframe = document.createElement('iframe');
+
+            // Set the source URL for the iframe
+            iframe.src = 'https://dantruongforgames.github.io/Argentine-flash-Games/madnessprojectnexus/';
+
+            // Set iframe attributes to cover the entire page
+            iframe.style.position = 'absolute';
+            iframe.style.top = '0';
+            iframe.style.left = '0';
+            iframe.style.width = '100%';
+            iframe.style.height = '100%';
+            iframe.style.border = 'none';
+
+            // Append the iframe to the new window's document
+            newWindow.document.body.appendChild(iframe);
+
+            // Change the tab title
+            newWindow.document.title = 'Madness combat | Argentine classic';
+        }
+
+        // Add a click event listener to the button
+        document.getElementById('mc').addEventListener('click', openEmbed);
